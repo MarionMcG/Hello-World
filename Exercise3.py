896a8b5722a53f4a2f11d4985e29fb9e7a178e7d
MarionMcG <35726325+MarionMcG@users.noreply.github.com>
2018-02-07 09:45:55 +0000

    Update Exercise3.py

--- Exercise3.py
+++ Exercise3.py
@@ -1,11 +1,13 @@
 #Marion McGowan, 6-2-2018
 #Collatz Conjecture
 
-n = int(input("Please enter an integer: "))
+n = int(input("Please enter a natural number: "))
 while n!=1:
     if n%2==0:
        n=(n/2)
     elif n%2==1:
        n=(3*n + 1)
     print(n)
-       
+    
+#This only works when n is a natural, number greater than 1. Negative numbers result in an infinite loop.
+#I thought I'd made a mistake, but Wikipedia page states the input should be a positive integer.
