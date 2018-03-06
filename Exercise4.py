1c01ff9d53efc4bac6dfaca3d7b1a0f19a939426
MarionMcG <35726325+MarionMcG@users.noreply.github.com>
2018-03-06 19:48:17 +0000

    Update Exercise4.py

--- Exercise4.py
+++ Exercise4.py
@@ -1,16 +1,17 @@
 
 #Marion McGowan 01/03/2018
 #Project Euler Problem 5 - Solution 1
-def multiply(numbers):  
-    total = 1
-    for x in numbers:
-        total *= x  
-    return total
+import time
+start_time = time.time()
+# Researched code to return time it took my program to run here, https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution
 
-#https://www.w3resource.com/python-exercises/python-functions-exercise-3.php
-#Link above gave definition of how to multiply list
+def prod(numbers):
+    prod=1
+    for i in range (1, 1*numbers):
+        prod =prod*i
+    return prod #Wrote this function based on instruction in Moodle video uploaded 05/03/2018
 
-max = (multiply((2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)))
+max = (prod(21))
 for p in range (21, max):
     if ((p%2==0) and (p%3==0) and (p%4==0) and (p%5==0) and (p%6==0) 
         and (p%7==0) and (p%8==0)and (p%9==0) and (p%10==0)
@@ -20,10 +21,10 @@ for p in range (21, max):
         break
 
 print("The smallest number divisible by each number from 1 to 20 is", p)
+print("My program took %s seconds to run" % int(time.time() - start_time))
 
 #I struggled with this problem and was unable to generalise it
 #I tried this; for p in range(21,max): for r in range(2, 21): 
 #               if p%r==0: print (p)
 #This method didnt correctly iterate through the range
-#It terminated at the first value in p divisible by only 2
 #Need to figure out how to iterate a loop over a range of values
