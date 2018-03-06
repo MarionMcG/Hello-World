db80bd9c06e6d18f3ac78658113086ca7439d021
MarionMcG <35726325+MarionMcG@users.noreply.github.com>
2018-03-06 20:41:16 +0000

    Add files via upload

---
+++ Exercise6.py
@@ -0,0 +1,15 @@
+#Marion McGowan, 06/03/2018
+#Write a Python script containing a function called factorial() that takes a single input/argument which is a positive integer and returns its factorial.
+#You should, in your script, test the function by calling it with the values 5, 7, and 10.
+
+def factorial(number):
+    factorial=1
+    number=number+1
+    for i in range (1, 1*number):
+        factorial=factorial*i
+    return factorial
+
+
+print(factorial(5))
+print(factorial(7))
+print(factorial(10))
